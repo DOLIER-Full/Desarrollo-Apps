7fc3ede48ea8fa6d88e2813ea0e8731d06a28363
C_P_S <cesitar.puentes@gmail.com>
2026-08-26 12:29:37 -0500

    Delete angular-rick-morty directory

--- angular-rick-morty/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { apiInterceptor } from './api.interceptor';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withInterceptors([apiInterceptor]))
-  ]
-};
